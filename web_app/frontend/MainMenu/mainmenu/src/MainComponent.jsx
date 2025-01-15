6d3e66c1d4a0e311dcb093c6a34c20f42a7bcf0c
Riviera DeSantos <niebieski.kwiat.i.kolce.2020@gmail.com>
2025-01-15 13:31:37 +0100

    Temperature chart has beed added

--- web_app/frontend/MainMenu/mainmenu/src/MainComponent.jsx
+++ web_app/frontend/MainMenu/mainmenu/src/MainComponent.jsx
@@ -3,6 +3,7 @@ import NoteComponent from './NoteComponent/NoteComponent.jsx';
 import {InitData} from './Context/InitContextData.jsx';
 import TimerComponent from './TimerComponent/TimerComponent.jsx';
 import PowerConsumptionComponent from './PowerConsumptionComponent/PowerConsumptionComponent.jsx';
+import ThermometerComponent from './ThermometerComponent/ThermometerComponent.jsx';
 
 function MainComponent() {
   return (
@@ -10,6 +11,16 @@ function MainComponent() {
     <InitData.Provider value={{
       noteJSON: {},
       runTimeJSON: { day: "01", month: "01", year: "2025", hh: "00", mm: "00", ss: "00" },
+
+      labelPerProbeJSON: [
+        "01.01.2025", "02.01.2025", "03.01.2025", "04.01.2025", "05.01.2025",
+        "06.01.2025", "07.01.2025", "08.01.2025", "09.01.2025", "10.01.2025",
+        "11.01.2025", "12.01.2025", "13.01.2025", "14.01.2025", "15.01.2025",
+        "16.01.2025", "17.01.2025", "18.01.2025", "19.01.2025", "20.01.2025",
+        "21.01.2025", "22.01.2025", "23.01.2025", "24.01.2025", "25.01.2025",
+        "26.01.2025", "27.01.2025", "28.01.2025", "29.01.2025", "30.01.2025"
+      ],
+
       powerConsumptionJSON: {
 
         labelPerProbe: [
@@ -22,10 +33,10 @@ function MainComponent() {
         ],
 
         totalPerProbe: [
-          13.82, 6.98, 7.49, 2.75, 12.1, 8.4, 13.31, 4.33, 13.99, 6.43,
-          8.67, 7.82, 8.24, 3.46, 9.02, 12.67, 4.1, 11.46, 5.35, 11.36,
-          4.86, 9.8, 9.6, 7.94, 13.48, 10.71, 13.26, 12.37, 8.26, 10.91
-        ],
+          28.82, 18.98, 34.49, 20.75, 34.1, 19.4, 29.31, 24.33, 27.99, 31.43,
+          38.67, 35.82, 29.24, 29.46, 22.02, 31.67, 21.1, 35.46, 34.35, 21.36,
+          18.86, 32.8, 29.6, 19.94, 43.48, 25.71, 39.26, 34.37, 26.26, 35.91
+          ],
 
         maxPerProbe: [
           0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00,
@@ -39,8 +50,18 @@ function MainComponent() {
           0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00
         ],
 
-        pricePerkWh: 1.30
+        pricePerkWh: 1.30,
+      
+      },
 
+      ThermometerJSON: 
+      {
+        last30TempProbe: [
+          28.82, 18.98, 34.49, 20.75, 34.1, 19.4, 29.31, 24.33, 27.99, 31.43,
+          38.67, 35.82, 29.24, 29.46, 22.02, 31.67, 21.1, 35.46, 34.35, 21.36,
+          18.86, 32.8, 29.6, 19.94, 43.48, 25.71, 39.26, 34.37, 26.26, 35.91
+
+        ],
       }
 
     }}>
@@ -55,7 +76,7 @@ function MainComponent() {
 
           </div>
           <div className="tempButton">
-
+              
           </div>
           <div className="tempButton">
 
@@ -91,7 +112,7 @@ function MainComponent() {
           </div>
 
           <div className="content2b">
-            
+           <ThermometerComponent/>
           </div>
 
           <div className="content2c">
