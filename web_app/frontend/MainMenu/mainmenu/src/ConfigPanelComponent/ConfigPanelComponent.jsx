96cda877a10be183588d6d46de6b042e82848749
Riviera DeSantos <niebieski.kwiat.i.kolce.2020@gmail.com>
2025-02-18 12:28:04 +0100

    Config Components is done in half, add bar with build-in programs for plants, make save, get and post funcionaity etc

--- web_app/frontend/MainMenu/mainmenu/src/ConfigPanelComponent/ConfigPanelComponent.jsx
+++ web_app/frontend/MainMenu/mainmenu/src/ConfigPanelComponent/ConfigPanelComponent.jsx
@@ -5,10 +5,15 @@ import SliderComponent from "../ConfigChartComponent/SliderComponent.jsx";
 export default function ConfigPanelComponent()
 {
     return(
-        <div className="ConfigPanelComponent">
+        <div className="ConfigPanelComponentCCC">
               {/* 3 columns one row */}
-              <div className="outerGridC"> 
-                <SliderComponent minY={0} maxY={100} startTime={0} endTime={60*60*24-1} chartLabel={'temperature'}/>
+              <div className="outerGridCCC">
+                <SliderComponent minY={0} maxY={100} startTime={0} endTime={60*60*24-1} chartLabel={'TEMPERATURE'}/>
+                <SliderComponent minY={0} maxY={100} startTime={0} endTime={60*60*24-1} chartLabel={'AIR HUMIDITY'}/>
+                <SliderComponent minY={0} maxY={100} startTime={0} endTime={60*60*24-1} chartLabel={'SOIL HUMIDITY'}/>
+                <SliderComponent minY={0} maxY={1000} startTime={0} endTime={60*60*24-1} chartLabel={'N'}/>
+                <SliderComponent minY={0} maxY={1000} startTime={0} endTime={60*60*24-1} chartLabel={'P'}/>
+                <SliderComponent minY={0} maxY={1000} startTime={0} endTime={60*60*24-1} chartLabel={'K'}/>
               </div>
             </div>
     )
