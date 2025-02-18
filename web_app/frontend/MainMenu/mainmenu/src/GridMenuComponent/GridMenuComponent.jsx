0f45f8680029209bd2d9e080919261911e63fcb4
Riviera DeSantos <niebieski.kwiat.i.kolce.2020@gmail.com>
2025-02-18 08:30:55 +0100

    nav menu like icons etc is ready

--- web_app/frontend/MainMenu/mainmenu/src/GridMenuComponent/GridMenuComponent.jsx
+++ web_app/frontend/MainMenu/mainmenu/src/GridMenuComponent/GridMenuComponent.jsx
@@ -5,6 +5,8 @@ import { ReactComponent as SettingSVG } from '../resources/settings.svg';
 import { ReactComponent as AccountSVG } from '../resources/account.svg';
 import { ReactComponent as DevicesSVG } from '../resources/devices.svg';
 import { ReactComponent as HomeSVG } from '../resources/home.svg';
+import { ReactComponent as LogOutSVG} from '../resources/logout.svg'
+
 export default function GridMenuComponent({ children }) {
     return (
         <div className="menuGridY">
@@ -16,22 +18,20 @@ export default function GridMenuComponent({ children }) {
                     </div>
             </NavLink>
 
-            <NavLink to="/config"
-                className={({ isActive }) => isActive ? "activeButton" : "normalButton"} >
-                    <div className="svg">
-                        <SettingSVG className="svgInner"/>
-                    </div>
-            </NavLink>
-
-
             <NavLink to="/devices"
                 className={({ isActive }) => isActive ? "activeButton" : "normalButton"} >
                     <div className="svg">
                         <DevicesSVG className="svgInner"/>
                     </div>
             </NavLink>
-            
 
+            <NavLink to="/config"
+                className={({ isActive }) => isActive ? "activeButton" : "normalButton"} >
+                    <div className="svg">
+                        <SettingSVG className="svgInner"/>
+                    </div>
+            </NavLink>
+            
             <NavLink to="/account"
                 className={({ isActive }) => isActive ? "activeButton" : "normalButton"} >
                     <div className="svg">
@@ -39,12 +39,16 @@ export default function GridMenuComponent({ children }) {
                     </div>
             </NavLink>
 
-            <div className="tempButton">
-            </div>
-
-            <div className="tempButton">
+            <div className="normalButton blank">
 
             </div>
+
+            <NavLink to="/login"
+                className={({ isActive }) => isActive ? "activeButton" : "normalButton"} >
+                    <div className="svg">
+                        <LogOutSVG className="svgInner"/>
+                    </div>
+            </NavLink>
         </div>
     )
 
