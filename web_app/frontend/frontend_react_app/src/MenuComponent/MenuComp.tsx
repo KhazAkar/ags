5fa061f913d5f42a78cbb0dec1c00bf14d9ba32a
Riviera DeSantos <niebieski.kwiat.i.kolce.2020@gmail.com>
2025-04-09 14:17:14 +0200

    Improved code of menu component

--- web_app/frontend/frontend_react_app/src/MenuComponent/MenuComp.tsx
+++ web_app/frontend/frontend_react_app/src/MenuComponent/MenuComp.tsx
@@ -1,60 +1,40 @@
 import React from 'react';
 import menuStyle from './MenuStyle.module.css';
 
-import { NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { ReactComponent as SettingSVG } from '../resources/svg/settings.svg';
 import { ReactComponent as AccountSVG } from '../resources/svg/account.svg';
 import { ReactComponent as DevicesSVG } from '../resources/svg/devices.svg';
 import { ReactComponent as HomeSVG } from '../resources/svg/home.svg';
-import { ReactComponent as LogOutSVG} from '../resources/svg/logout.svg';
+import { ReactComponent as LogOutSVG } from '../resources/svg/logout.svg';
+
+function MenuButton({ path, IconSVG }: {path: string, IconSVG: React.FC<React.SVGProps<SVGSVGElement>>}) {
+  return (<NavLink to={path} end
+    className={({ isActive }) => isActive ? menuStyle.activeButton : menuStyle.normalButton} >
+    <div className={menuStyle.svg}>
+      <IconSVG className={menuStyle.svgInner} />
+    </div>
+  </NavLink>
+  )
+}
 
 export default function MenuComp() {
-    return (
-      <div className={menuStyle.container}>
-        <div className={menuStyle.menuWrapperGrid}>
+  return (
+    <div className={menuStyle.container}>
+      <div className={menuStyle.menuWrapperGrid}>
 
-            <NavLink to="/app" end
-                className={({ isActive }) => isActive ? menuStyle.activeButton : menuStyle.normalButton} >
-                    <div className={ menuStyle.svg }>
-                        <HomeSVG className={menuStyle.svgInner}/>
-                    </div>
-            </NavLink>
+        <MenuButton path="/app" IconSVG={HomeSVG} />
+        <MenuButton path="devices" IconSVG={DevicesSVG} />
+        <MenuButton path="config" IconSVG={SettingSVG} />
+        <MenuButton path="account" IconSVG={AccountSVG} />
 
-            <NavLink to="devices" end
-                className={({ isActive }) => isActive ? menuStyle.activeButton  :  menuStyle.normalButton} >
-                    <div className={ menuStyle.svg }>
-                        <DevicesSVG className={menuStyle.svgInner}/>
-                    </div>
-            </NavLink>
-
-            <NavLink to="config" end
-                className={({ isActive }) => isActive ? menuStyle.activeButton  :  menuStyle.normalButton} >
-                    <div className={ menuStyle.svg }>
-                        <SettingSVG className={menuStyle.svgInner}/>
-                    </div>
-            </NavLink>
-            
-            <NavLink to="account" end
-                className={({ isActive }) => isActive ? menuStyle.activeButton  :  menuStyle.normalButton} >
-                    <div className={ menuStyle.svg }>
-                        <AccountSVG className={menuStyle.svgInner}/>
-                    </div>
-            </NavLink>
-
-            <div className={menuStyle.normalButtonX  }>
-
-            </div>
-
-            <NavLink to="/" end
-                className={({ isActive }) => isActive ? menuStyle.activeButton  :  menuStyle.normalButton} >
-                    <div className={ menuStyle.svg }>
-                        {//Here add some logic to log out the server and close the connection
-                        }
-                        <LogOutSVG className={menuStyle.svgInner}/>
-                    </div>
-            </NavLink>
+        <div>
+          {/*blank div because of my idea for that component in css xdxdxd */}
         </div>
-        </div>
-    )
+        
+        <MenuButton path="/" IconSVG={LogOutSVG} /> {/*Here add some logic to log out the server and close the connection*/}
+      </div>
+    </div>
+  )
 
 }
